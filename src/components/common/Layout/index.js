d30ede7d2700aea812e5759d662b2e01f05afee9
adhitamafikri <adhitamafikri@gmail.com>
2019-09-18 22:55:09 +0700

    updated layout

--- src/components/common/Layout/index.js
+++ src/components/common/Layout/index.js
@@ -5,7 +5,16 @@ function Layout({ children, header, navigation }) {
   return (
     <Pane>
       {header && header.component}
-      <Pane width="100%" height="100vh" paddingTop={52} paddingBottom={52}>{children}</Pane>
+      <Pane
+        width="100%"
+        height="100vh"
+        paddingTop={68}
+        paddingBottom={68}
+        paddingLeft={16}
+        paddingRight={16}
+      >
+        {children}
+      </Pane>
       {navigation && navigation.component}
     </Pane>
   )
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,19 +1,30 @@
 import React from 'react'
 
-import { postLogin } from '../../services/auth'
+import Layout from 'components/common/Layout'
+import Header from 'components/common/Header'
+import Navigation from 'components/common/Navigation'
+
+import { postLogin } from 'services/auth'
 
 function Login() {
   const [username, setUsername] = React.useState('')
   const [password, setPassword] = React.useState('')
 
-  const handleFormSubmit = e => {
+  const handleFormSubmit = (e) => {
     e.preventDefault()
     console.log('submitting form', username, password)
     postLogin({ username, password })
   }
 
   return (
-    <div>
+    <Layout
+      header={{
+        component: <Header />,
+      }}
+      navigation={{
+        component: <Navigation />,
+      }}
+    >
       Sociozone Login
       <form method="POST" onSubmit={handleFormSubmit} autoComplete="off">
         <label htmlFor="uname">Username</label>
@@ -22,7 +33,7 @@ function Login() {
           placeholder="username"
           name="username"
           id="uname"
-          onChange={e => setUsername(e.target.value)}
+          onChange={(e) => setUsername(e.target.value)}
         />
         <label htmlFor="pwd">Password</label>
         <input
@@ -30,12 +41,12 @@ function Login() {
           placeholder="username"
           name="password"
           id="pwd"
-          onChange={e => setPassword(e.target.value)}
+          onChange={(e) => setPassword(e.target.value)}
         />
 
         <input type="submit" value="Submit" />
       </form>
-    </div>
+    </Layout>
   )
 }
 
