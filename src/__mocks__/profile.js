840ce53176630b4841a109448216971244a7f7b3
adhitamafikri <adhitamafikri@gmail.com>
2019-09-20 17:44:09 +0700

    profile page scaffolding

---
+++ src/__mocks__/profile.js
@@ -0,0 +1,7 @@
+export default {
+  id: 1,
+  username: 'roxanneroxanne',
+  name: 'Roxanne',
+  bio: 'magna summis veniam quem amet anim sint quid quem dolore minim fore',
+  website: 'codepen.io/adhitamafikri',
+}
--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,10 +1,14 @@
 import React from 'react'
-import { Pane, Heading, Button } from 'evergreen-ui'
+import {
+  Pane, Text, Paragraph, Link,
+} from 'evergreen-ui'
 
 import Layout from 'components/common/Layout'
 import Header from 'components/common/Header'
 import Navigation from 'components/common/Navigation'
-import Link from 'components/common/Link'
+import Image from 'components/common/Image'
+
+import profileData from '__mocks__/profile'
 
 function Profile() {
   return (
@@ -16,7 +20,36 @@ function Profile() {
         component: <Navigation />,
       }}
     >
-      <Pane>Profile</Pane>
+      <Pane>
+        <Pane
+          width={70}
+          height={70}
+          borderRadius={50}
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          overflow="hidden"
+        >
+          <Image src="https://i.ibb.co/tcWVbVP/Jorja-Smith.jpg" width="auto" height="100%" fit="cover" />
+        </Pane>
+      </Pane>
+      <Pane
+        display="flex"
+        flexDirection="column"
+      >
+        <Text fontWeight={500} fontSize={18} marginTop={8} marginBottom={4}>
+          {profileData.name}
+        </Text>
+        <Text fontWeight={500} marginTop={4} marginBottom={4}>
+          {`@${profileData.username}`}
+        </Text>
+        <Paragraph marginTop={4} marginBottom={4}>
+          {profileData.bio}
+        </Paragraph>
+        <Link href={profileData.website} fontSize={13}>
+          {profileData.website}
+        </Link>
+      </Pane>
     </Layout>
   )
 }
